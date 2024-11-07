ff790c8e44f812647417b96ac0f1ab415b476794
Rich <applerich0306@gmail.com>
2024-11-08 00:10:32 +0200

    add token expire login in frontend

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,14 +4,41 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './global.css';
 import { User } from '@/types/type_User';
-import { useState } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { useState, useEffect } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from 'next/router';
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps)
 {
 	const [user, setUser] = useState<User | null>(null);
 
+	const router = useRouter();
+
 	console.log(user)
+
+	const checkTokenExpiration = () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken: { exp: number } = jwtDecode(token);
+      const isExpired = decodedToken.exp * 1000 < Date.now();
+      if (isExpired) {
+        localStorage.removeItem('token');
+        setUser(null);
+        toast.error("Session expired. Please log in again.");
+				router.push('/login');
+      }
+    }
+	};
+
+	useEffect(() => {
+    checkTokenExpiration();
+
+    const interval = setInterval(checkTokenExpiration, 5 * 60 * 1000);
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
 
   return (
     <SessionProvider session={session}>
